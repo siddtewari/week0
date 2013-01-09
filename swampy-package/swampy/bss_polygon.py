aa94709b9a8c62779eca45698f88c5832c2299b5
siddtewari <sidtewari@gmail.com>
2013-01-09 18:11:37 -0500

    Delay reduced. Other tests included.

--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -1,28 +1,49 @@
 # Polygon excercise from Week 0
-
 # Name: Sidd Tewari
 
 import math
 from TurtleWorld import * 
 world = TurtleWorld()			
-lee = Turtle()				
+
+lee = Turtle()
+lee.delay = 0.02			
 print lee
 
+def setup(Turtle):
+	world.clear()
+	# pu(Turtle)
+	# rt(Turtle)
+	# fd(Turtle,100)
+	# rt(Turtle)
+	# pd(Turtle)
+
+
 def polygon(Turtle,len,n):
+	setup(Turtle)
 	angle = 360.0/n
 	for i in range(n):
 		fd(Turtle,len)
 		lt(Turtle,angle)
 
 def circle(t,r):
-	world.clear()
 	pi = math.pi
 	circum = 2*pi*r
-	length = r/5 		        # hard coded approximation for length
+	length = r/5		        # hard-coded approximation 
 	sides = int(circum/length)  # circum == length * sides
-	polygon(lee,length,sides)
+	polygon(t,length,sides)
 	
+polygon(lee,100,3)
+polygon(lee,150,4)
+polygon(lee,50,5)
+polygon(lee,50,6)
+polygon(lee,60,8)
+
+#polygon(lee,50,13)
+
 circle(lee,23)
 circle(lee,50)
-cirle(lee,500)
+circle(lee,100)
+#circle(lee,200)
+#circle(lee,1000)
+#circle(lee,5000)
 wait_for_user()					
