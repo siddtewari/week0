846b0619565b008f932d4ec38baee9494a10699b
siddtewari <sidtewari@gmail.com>
2013-01-10 10:05:45 -0500

    Refactoring; 2 new functions, draw() & circularCalculations(). Return tuple introduced!

--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -8,28 +8,29 @@ lee = Turtle()
 lee.delay = 0.02
 print lee
 
-def polygon(Turtle,len,sides):
-	world.clear()
+def polygon(Turtle,length,sides):
 	angle = 360.0/sides
-	for i in range(sides):
-		fd(Turtle,len)
-		lt(Turtle,angle)
+	draw(Turtle,length,sides,angle)
 
 def circle(Turtle,radius):
-	world.clear()
-	pi = math.pi
-	circum = 2*pi*radius
-	length = radius/5		    # hard-coded approximation 
-	sides = int(circum/length)  # circum == length * sides
-	polygon(Turtle,length,sides)
+	length, sides = circularCalculations(Turtle,radius)
+	angle = 360.0/sides
+	draw(Turtle,length,sides,angle)
 
 def arc(Turtle,radius,theta):
-	world.clear()
+	length, sides = circularCalculations(Turtle,radius)
+	angle = theta/sides
+	draw(Turtle,length,sides,angle)
+
+def circularCalculations(Turtle,radius):
 	pi = math.pi
 	circum = 2*pi*radius
 	length = radius/5		    # hard-coded approximation 
 	sides = int(circum/length)  # circum == length * sides
-	angle = theta/sides
+	return (length,sides)
+
+def draw(Turtle,length,sides,angle):
+	world.clear()
 	for i in range(sides):
 		fd(Turtle,length)
 		lt(Turtle,angle)
@@ -37,14 +38,11 @@ def arc(Turtle,radius,theta):
 arc(lee,10,360)
 arc(lee,30,90)
 arc(lee,30,180)
-
-	
 polygon(lee,100,3)
 polygon(lee,150,4)
 polygon(lee,50,5)
 polygon(lee,50,6)
 polygon(lee,60,8)
-
 circle(lee,23)
 circle(lee,50)
 circle(lee,100)
