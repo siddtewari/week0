c94062c0fa879d6117dbc1b109c91f499f38f298
siddtewari <sidtewari@gmail.com>
2013-01-09 23:59:34 -0500

    Arc function added with theta as a parameter

--- swampy-package/swampy/bss_polygon.py
+++ swampy-package/swampy/bss_polygon.py
@@ -3,34 +3,41 @@
 
 import math
 from TurtleWorld import * 
-world = TurtleWorld()			
-
+world = TurtleWorld()
 lee = Turtle()
-lee.delay = 0.02			
+lee.delay = 0.02
 print lee
 
-def setup(Turtle):
+def polygon(Turtle,len,sides):
 	world.clear()
-	# pu(Turtle)
-	# rt(Turtle)
-	# fd(Turtle,100)
-	# rt(Turtle)
-	# pd(Turtle)
-
-
-def polygon(Turtle,len,n):
-	setup(Turtle)
-	angle = 360.0/n
-	for i in range(n):
+	angle = 360.0/sides
+	for i in range(sides):
 		fd(Turtle,len)
 		lt(Turtle,angle)
 
-def circle(t,r):
+def circle(Turtle,radius):
+	world.clear()
 	pi = math.pi
-	circum = 2*pi*r
-	length = r/5		        # hard-coded approximation 
+	circum = 2*pi*radius
+	length = radius/5		    # hard-coded approximation 
 	sides = int(circum/length)  # circum == length * sides
-	polygon(t,length,sides)
+	polygon(Turtle,length,sides)
+
+def arc(Turtle,radius,theta):
+	world.clear()
+	pi = math.pi
+	circum = 2*pi*radius
+	length = radius/5		    # hard-coded approximation 
+	sides = int(circum/length)  # circum == length * sides
+	angle = theta/sides
+	for i in range(sides):
+		fd(Turtle,length)
+		lt(Turtle,angle)
+
+arc(lee,10,360)
+arc(lee,30,90)
+arc(lee,30,180)
+
 	
 polygon(lee,100,3)
 polygon(lee,150,4)
@@ -38,12 +45,8 @@ polygon(lee,50,5)
 polygon(lee,50,6)
 polygon(lee,60,8)
 
-#polygon(lee,50,13)
-
 circle(lee,23)
 circle(lee,50)
 circle(lee,100)
-#circle(lee,200)
-#circle(lee,1000)
-#circle(lee,5000)
+
 wait_for_user()					
